ce42a6a290eb53a028b5da19b9f8107ccc025952
kadir balçın <kadirbalcin145@gmail.com>
2023-12-12 19:57:06 +0300

    second commit

---
+++ app/src/App.jsx
@@ -0,0 +1,14 @@
+import {AdminLayout} from '@uvodohq/admin-ui'
+import {H1} from '@uvodohq/planum'
+import '@uvodohq/admin-ui/dist/style.css'
+
+function App() {
+
+    return (
+        <AdminLayout>
+          <H1>Your Plugin Name</H1>
+        </AdminLayout>
+    )
+}
+
+export default App
